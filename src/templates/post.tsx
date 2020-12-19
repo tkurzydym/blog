c5cd9cabaedb70beaef09a55edab269308bf86b3
tkurzydym <tobias.kurzydym@protonmail.com>
2020-12-19 14:56:25 +0100

    added scroll to top icon

--- src/templates/post.tsx
+++ src/templates/post.tsx
@@ -2,10 +2,25 @@ import React from "react"
 import Layout from "../components/layout/layout"
 import SEO from "../components/layout/seo"
 import Prism from "prismjs"
+import styled  from "styled-components"
 
 import { graphql } from "gatsby"
 import { useEffect } from "react"
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faCaretSquareUp as upReg } from "@fortawesome/free-regular-svg-icons"
+
+const ScrollTopIcon = styled.div`
+  float: right;
+  padding-bottom: 2rem;
+  &:hover {
+    cursor: pointer;
+  }
+`
+
+function scrollToTop() {
+    window.scrollTo({top: 0,left: 0, behavior: 'smooth'})
+}
 
 export default function PostTemplate({ data }) {
 
@@ -17,6 +32,10 @@ export default function PostTemplate({ data }) {
     <Layout>
       <SEO title={data.asciidoc.document.title} />
         <div dangerouslySetInnerHTML={{ __html: data.asciidoc.html }}/>
+
+        <ScrollTopIcon>
+          <FontAwesomeIcon size='2x' icon={upReg} onClick={scrollToTop}></FontAwesomeIcon>
+        </ScrollTopIcon>
     </Layout>
   )
 }
