4b879d60b6e9a926d7edb704b061ac8c2580fd3e
tkurzydym <tobias.kurzydym@protonmail.com>
2021-07-31 22:24:54 +0200

    fix(about): revert about page change

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,22 +1,12 @@
 import React from "react"
 import Layout from "../components/layout/layout"
 import SEO from "../components/layout/seo"
-import { StaticImage } from "gatsby-plugin-image"
 
 export default function AboutPage() {
   return (
     <Layout>
       <SEO title="About" />
 
-      <StaticImage
-        src="../images/about-profile-picture.jpg"
-        alt="Profile Picture"
-        placeholder="blurred"
-        layout="fixed"
-        height={200}
-        width={200}
-        
-      ></StaticImage>
     </Layout>
   )
 }
