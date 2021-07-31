f7cfbfb7c04b719518add4f7d3a8a7129209d081
tkurzydym <tobias.kurzydym@protonmail.com>
2021-07-31 13:53:52 +0200

    fix(blog): import case

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import React from "react"
-import Layout from "../components/layout/Layout"
-import SEO from "../components/layout/SEO"
+import Layout from "../components/layout/layout"
+import SEO from "../components/layout/seo"
 import RecentPosts from "../components/blog/recent-posts"
 
 export default function IndexPage() {
